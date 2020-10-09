9bdf835d424c180fbab015ceac5541e7a12cf2e2
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-09 09:39:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Compares two arrays
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    // declare both arrays and the array length constant
+    final int ARRAY_LENGTH = 5;
+    int[] firstNumbers = new int[ARRAY_LENGTH];
+    int[] secondNumbers = new int[ARRAY_LENGTH];
+
+    // ask for the numbers in the first arrray
+    System.out.println("Please enter the values for the first array:");
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      firstNumbers[i] = input.nextInt();
+    }
+
+    // ask for the numbers in the second array
+    System.out.println("Please enter the values for the second array:");
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      secondNumbers[i] = input.nextInt();
+    }
+
+    // compares arrays, if the numbers are the same go onto the next pair of indexes, if not tell the user and end the program
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      if (firstNumbers[i] != secondNumbers[i]) {
+        System.out.println("These arrays are not the same");
+        System.exit(0);
+      }
+    }
+
+    // tell the user if the the arrays are the same    
+    System.out.println("These arrays are the same");
+
   }
 }
